bd65be4da473878e6a965ada1a63c9b976dcc4d4
Sergei Shevchenko <wunder49@yandex.by>
2023-11-25 22:56:43 +0300

    solution for reverseString

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -249,8 +249,9 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  // throw new Error('Not implemented');
+  return str.split('').reverse().join('');
 }
 
 /**
