2b98fb677af109732272a6b68f0dffd9af27ee5c
Sergei Shevchenko <wunder49@yandex.by>
2023-11-25 22:55:53 +0300

    solution for endsWith

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -217,8 +217,9 @@ function startsWith(str, substr) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  // throw new Error('Not implemented');
+  return str.endsWith(substr);
 }
 
 /**
