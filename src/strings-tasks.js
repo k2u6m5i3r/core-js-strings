11c720c2d13103b71c7a295c0e482f00cf89ee57
Sergei Shevchenko <wunder49@yandex.by>
2023-11-25 22:50:10 +0300

    solution for extractNameFromTemplate

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -406,8 +406,9 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  // throw new Error('Not implemented');
+  return value.slice(7, -1);
 }
 
 /**
